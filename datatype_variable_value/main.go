cbf08d2da0476c21b4e7cc03b51fe6bb63a8d585
sirrush <thaibao@autonomous.nyc>
2019-07-09 21:57:54 +0700

    init

--- datatype_variable_value/main.go
+++ datatype_variable_value/main.go
@@ -15,3 +15,16 @@ func main() {
 	var isGoodWebsite = true
 	fmt.Println("isGoodwebsite = ", isGoodWebsite)
 }
+
+//func main() {
+//	var x1, x2, x3 int = 21, 6, 1987
+//	fmt.Println("x1 = ", x1)
+//	fmt.Println("x2 = ", x2)
+//	fmt.Println("x3 = ", x3)
+//}
+
+//func main() {
+//	// this is a short declaring variable and value, omit data type because you knew what it is
+//	thisAStringVariableMyEmail := "thaibao56@gmail.com"
+//	fmt.Println("I will print this varible for you:", thisAStringVariableMyEmail)
+//}
