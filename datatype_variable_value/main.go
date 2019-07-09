3321e41eb0940c7589ae69167d67fba5f730d6b9
sirrush <thaibao@autonomous.nyc>
2019-07-09 21:40:59 +0700

    init

---
+++ datatype_variable_value/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 100
+	fmt.Println("x = ", x)
+
+	var y = 19.9
+	fmt.Println("y = ", y)
+
+	var myName = "thaibao"
+	fmt.Println("myName = ", myName)
+
+	var isGoodWebsite = true
+	fmt.Println("isGoodwebsite = ", isGoodWebsite)
+}
